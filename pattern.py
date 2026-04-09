9cabf93c48870b973a10d23dd38d5eb94d10a327
Narasimha741 <112624154+Narasimha741@users.noreply.github.com>
2026-04-09 23:35:23 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,71 @@
+#right angled triangle left aligned
+# n=5
+# for i in range(1, n+1):
+#     for j in range(1, i+1): print("*",end=" ")
+#     for k in range(n-i): print(" ",end=" ")
+#     print()
+
+#inverted right angled triangle left aligned
+# n=5
+# for i in range(n, 0, -1):
+#     for j in range(1, i+1): print("*",end=" ")
+#     for k in range(n-i): print(" ",end=" ")
+#     print()
+
+# right angled triangle right aligned
+# n=5
+# for i in range(1, n+1):
+#     for j in range(n-i): print(" ",end=" ")
+#     for k in range(1, i+1): print("*",end=" ")
+#     print()
+
+
+# inverted right angled triangle right aligned
+# n=5
+# for i in range(n, 0, -1):
+#     for j in range(n-i): print(" ",end=" ")
+#     for k in range(1, i+1): print("*",end=" ")
+#     print()
+
+
+
+# hollo right angled triangle left aligned
+# n=5
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         if i==n or j==1 or i==j: print("*",end=" ")
+#         else: print(" ",end=" ")
+#     for k in range(n-i): print(" ",end=" ")
+#     print()
+
+# inverted hollow right angled triangle left aligned
+# n=5
+# for i in range(n, 0, -1):
+#     for j in range(1, i+1):
+#         if i==n or j==1 or i==j: print("*",end=" ")
+#         else: print(" ",end=" ")
+#     for k in range(n-i): print(" ",end=" ")
+#     print()
+
+
+# hollo right angled triangle right aligned
+# n=5
+# for i in range(1, n+1):
+#     for j in range(n-i): print(" ",end=" ")
+#     for k in range(1, i+1):
+#         if i==n or k==1 or i==k:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+#inverted hollo right angled triangle right aligned
+# n=5
+# for i in range(n , 0, -1):
+#     for j in range(n-i): print(" ",end=" ")
+#     for j in range(1, i+1):
+#         if i==n or j==1 or i==j:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
